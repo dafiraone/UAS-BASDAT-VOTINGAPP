ad4d9e27f9c33b9290b6a3f7d16f66e8e0777dcd
dafiraone <irawandaffa7@gmail.com>
2023-06-11 16:40:10 +0700

    change prisma

--- src/app/api/voteAction/route.ts
+++ src/app/api/voteAction/route.ts
@@ -2,9 +2,9 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-    const { session, voteId} = await req.json()
+    const { session, voteId } = await req.json()
     console.log(session, voteId)
-    await prisma.$queryRaw`UPDATE user SET status_pilih = false WHERE email = ${session}`
-    await prisma.$queryRaw`UPDATE pilihan SET jumlah_pemilih = jumlah_pemilih + 1 where id = ${voteId}`
-    return NextResponse.json({voteId}, {status: 200})
+    await prisma.$queryRaw`UPDATE User SET status_pilih = false WHERE email = ${session}`
+    await prisma.$queryRaw`UPDATE Pilihan SET jumlah_pemilih = jumlah_pemilih + 1 where id = ${voteId}`
+    return NextResponse.json({ voteId }, { status: 200 })
 }
